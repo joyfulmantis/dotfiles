5e4583d33a40d7cb2a5c268881fa38dfa1d0f212
Nathan Maxson <joyfulmantis@gmail.com>
2014-10-23 09:31:47 +0300

    refactored taffybar config

--- .config/taffybar/taffybar.hs
+++ .config/taffybar/taffybar.hs
@@ -1,21 +1,24 @@
-import Data.Monoid ( mconcat )
+import           Data.Monoid                              (mconcat)
 
-import Solarized
+import           Solarized
 
-import System.Taffybar
+import           System.Taffybar
+import           System.Taffybar.Battery
+import           System.Taffybar.FreedesktopNotifications
+import           System.Taffybar.Pager                    (colorize, escape,
+                                                           shorten, wrap)
+import           System.Taffybar.SimpleClock
+import           System.Taffybar.Systray
+import           System.Taffybar.TaffyPager
+import           System.Taffybar.Widgets.PollingLabel
 
-import System.Taffybar.Battery
-import System.Taffybar.Systray
-import System.Taffybar.Pager (colorize, escape, shorten, wrap)
-import System.Taffybar.Widgets.PollingLabel
-import System.Taffybar.TaffyPager
-import System.Taffybar.SimpleClock
-import System.Taffybar.FreedesktopNotifications
+import qualified Data.Text                                as T
 
-import qualified Data.Text as T
 
 myNotificationFormatter :: Notification -> String
-myNotificationFormatter note = wrap " :: " " :: " (colorize solarizedBase03 solarizedRed (escape (shorten 45 msg)))
+myNotificationFormatter note = wrap " :: " " :: "
+                               $ colorize solarizedBase03 solarizedRed
+                               $ escape $ shorten 45 msg
   where msg = case T.null (noteBody note) of
           True  -> T.unpack $ noteSummary note
           False -> T.unpack $ mconcat [ noteSummary note, noteBody note ]
@@ -23,28 +26,32 @@ myNotificationFormatter note = wrap " :: " " :: " (colorize solarizedBase03 sola
 mySep = colorize solarizedBase1 solarizedBase3 (escape " :: ")
 
 main = do
-  let pager         = taffyPagerNew defaultPagerConfig
-           { activeWindow      = colorize solarizedGreen    solarizedBase3.escape.shorten 45
-           , activeLayout      = colorize solarizedBlue     solarizedBase3.escape
-           , activeWorkspace   = colorize solarizedGreen    solarizedBase3.escape
-           , hiddenWorkspace   = colorize solarizedMagenta  solarizedBase3.escape
-           , emptyWorkspace    = colorize solarizedBase0    solarizedBase3.escape
-           , visibleWorkspace  = colorize solarizedBlue     solarizedBase3.escape
-           , urgentWorkspace   = colorize solarizedRed      solarizedBase3.escape
-           , widgetSep         = mySep }
-      clock         = textClockNew Nothing (colorize solarizedOrange solarizedBase3  "%d/%m/%y @ %H:%M" ++ mySep) 60
-      battery       = textBatteryNew (colorize solarizedBlue solarizedBase3 "$percentage$%" ++ mySep) 60
-      tray          = systrayNew
-      notifications = notifyAreaNew defaultNotificationConfig { notificationFormatter = myNotificationFormatter }
-  defaultTaffybar defaultTaffybarConfig { barHeight     = 19
-                                        , endWidgets    = [ tray, clock, battery, notifications ]
-                                        , startWidgets  = [ pager ]
-                                        , widgetSpacing = 0
-                                        }
-
-
-           -- , activeWorkspace   = colorize solarizedBase3 solarizedMagenta.escape
-           -- , hiddenWorkspace   = colorize solarizedBase3 solarizedOrange.escape
-           -- , emptyWorkspace    = colorize solarizedBase3 solarizedGreen.escape
-           -- , visibleWorkspace  = colorize solarizedBase3 solarizedBlue.escape
-           -- , urgentWorkspace   = colorize solarizedBase3 solarizedRed.escape
+  defaultTaffybar defaultTaffybarConfig
+    { barHeight     = 19
+    , endWidgets    = [ tray, clock, battery, notifications ]
+    , startWidgets  = [ pager ]
+    , widgetSpacing = 0 }
+  where pager = taffyPagerNew defaultPagerConfig
+                { activeWindow = colorize solarizedGreen solarizedBase3
+                                 . escape . shorten 45
+                , activeLayout = colorize solarizedBlue solarizedBase3
+                                 . escape
+                , activeWorkspace = colorize solarizedGreen solarizedBase3
+                                    . escape
+                , hiddenWorkspace = colorize solarizedMagenta solarizedBase3
+                                    . escape
+                , emptyWorkspace = colorize solarizedBase0 solarizedBase3
+                                   . escape
+                , visibleWorkspace = colorize solarizedBlue solarizedBase3
+                                    . escape
+                , urgentWorkspace = colorize solarizedRed solarizedBase3
+                                    . escape
+                , widgetSep = mySep }
+        clock = textClockNew Nothing
+                             (colorize solarizedOrange solarizedBase3
+                                       "%d/%m/%y @ %H:%M" ++ mySep) 60
+        battery = textBatteryNew (colorize solarizedBlue solarizedBase3
+                                           "$percentage$%" ++ mySep) 60
+        tray = systrayNew
+        notifications = notifyAreaNew defaultNotificationConfig
+                        { notificationFormatter = myNotificationFormatter }
